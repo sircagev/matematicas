2b333581acd42645abff2d7c131d679fd8232e66
sircagev <cf.giron04@gmail.com>
2022-12-08 22:12:08 -0500

    Cambios en Marcha, se agrgo todas las funciones y se empezo a rediseñar la pagina

--- js/perimetros YAreas.js
+++ js/perimetros YAreas.js
@@ -39,6 +39,44 @@ function enviarrom(){
     document.getElementById('respuesta-rom').innerHTML="El perimetro es "+peri+", el área es "+area
 }
 
+function enviartra(){
+    let baseMa, baseMe, lado1, lado2, altura, peri, area
+    baseMa = document.getElementById('base-mayor').value
+    baseMe = document.getElementById('base-menor').value
+    lado1 = document.getElementById('lado1-tra').value
+    lado2 = document.getElementById('lado2-tra').value
+    altura = document.getElementById('alturatra').value
+    peri = parseFloat(baseMa)+parseFloat(baseMe)+parseFloat(lado1)+parseFloat(lado2)
+    area = altura*(parseFloat(baseMa)+parseFloat(baseMe))/2
+    document.getElementById('respuesta-tra').innerHTML="El perimetro es "+peri+", el área es "+ area
+}
+
+function enviarcirc(){
+    let radio, peri, area
+    radio = document.getElementById('radio').value
+    peri = 2*radio*Math.PI
+    area = Math.PI*radio**2
+    document.getElementById('respuesta-circ').innerHTML="El perimetro es "+peri+", el área es "+area
+}
+
+function enviaresf(){
+    let radio, peri , area, volumen
+    radio = document.getElementById('radio-esf').value
+    peri = 2*radio*Math.PI
+    area = 4*Math.PI*radio**2
+    volumen = (4/3)*Math.PI*radio**3
+    document.getElementById('respuesta-esf').innerHTML="El perimetro es "+peri+", el área es "+area+", el volumen es "+volumen
+}
+
+function enviarcubo(){
+    let lado, peri, area, volumen
+    lado = document.getElementById('lado-cubo').value
+    peri = lado*12
+    area = 6*lado**2
+    volumen = lado**3
+    document.getElementById('respuesta-cubo').innerHTML="El perimetro es "+peri+", el área es "+area+", el volumen es "+volumen
+}
+
 function enviarp(){
     let alturap, anchop, largop, perimetrop,areap,volumenp
     alturap = document.getElementById('alturap').value
@@ -49,4 +87,23 @@ function enviarp(){
     volumenp = alturap*anchop*largop
     console.log()
     document.getElementById('respuestap').innerHTML="El perimetro es "+perimetrop+", el area es "+areap+", el volumen es "+volumenp
+}
+
+function enviarcilin(){
+    let altura, radio, peri, area, volumen
+    altura = document.getElementById('altura-cilindro').value
+    radio = document.getElementById('radio-cilindro').value
+    peri = 1221
+    area = 2*Math.PI*radio*(parseFloat(radio)+parseFloat(altura))
+    volumen =Math.PI*altura*radio**2
+    document.getElementById('respuesta-cilindro').innerHTML="El perimetro es "+peri+", el area es "+area+", el volumen es "+volumen
+}
+function enviarcono(){
+    let altura, radio, peri, area, volumen
+    altura = document.getElementById('altura-cono').value
+    radio = document.getElementById('radio-cono').value
+    peri = 1221
+    area = Math.PI*radio*(parseFloat(radio)+Math.sqrt(radio**2 + altura**2))
+    volumen =(Math.PI*altura*radio**2)/3
+    document.getElementById('respuesta-cono').innerHTML="El perimetro es "+peri+", el area es "+area+", el volumen es "+volumen
 }
